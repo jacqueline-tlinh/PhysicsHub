ef86a59e66b1cc72baecd553fa5ba980acbf87eb
jacqueline-tlinh <jqline.ng@gmail.com>
2026-01-04 20:26:30 +0700

    Fixed database errors, added a mock document in firestore database for testing, updated Upload Exam paper UI

--- app/src/main/java/com/example/physicshub/data/model/ExamMetadata.kt
+++ app/src/main/java/com/example/physicshub/data/model/ExamMetadata.kt
@@ -2,15 +2,15 @@ package com.example.physicshub.data.model
 
 data class ExamMetadata(
     val title: String = "",
-    val divisions: List<DivisionMetadata> = emptyList()
-)
-
-data class DivisionMetadata(
-    val name: String,
-    val categories: List<CategoryMetadata>
+    val categories: List<CategoryMetadata> = emptyList()
 )
 
 data class CategoryMetadata(
-    val name: String,
-    val courses: List<String>
+    val name: String = "",
+    val courses: List<CourseMetadata> = emptyList()
+)
+
+data class CourseMetadata(
+    val name: String = "",
+    val courseID: String = ""
 )
